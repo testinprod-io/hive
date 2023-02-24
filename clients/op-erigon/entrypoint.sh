0369f73e39f7fbe20ee8d76438cb26b3c8cf308c
pcw109550 <pcw109550@gmail.com>
2023-02-25 00:57:48 +0900

    Add op-erigon client

---
+++ clients/op-erigon/entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -exu
+
+# note: geth wants an integer log level (see L1 hive definition)
+VERBOSITY=${HIVE_ETH1_LOGLEVEL:-3}
+
+ERIGON_DATA_DIR=/db
+ERIGON_CHAINDATA_DIR="$ERIGON_DATA_DIR/geth/chaindata"
+
+CHAIN_ID=$(cat /genesis.json | jq -r .config.chainId)
+
+if [ ! -d "$ERIGON_CHAINDATA_DIR" ]; then
+	echo "$ERIGON_CHAINDATA_DIR missing, running init"
+	echo "Initializing genesis."
+	/usr/local/bin/erigon --log.console.verbosity="$VERBOSITY" init \
+		--datadir="$ERIGON_DATA_DIR" \
+		"/genesis.json"
+else
+	echo "$ERIGON_CHAINDATA_DIR exists."
+fi
+
+# We must set miner.gaslimit to the gas limit in genesis
+# in the command below!
+GAS_LIMIT_HEX=$(jq -r .gasLimit < /genesis.json | sed s/0x//i | tr '[:lower:]' '[:upper:]')
+GAS_LIMIT=$(echo "obase=10; ibase=16; $GAS_LIMIT_HEX" | bc)
+
+# erigon ALWAYS sets websocket port equal to its http port: 8545,
+# while others(op-batcher) try to communicate with erigon via 8546.
+# To overcome this issue, proxy requests using socat
+socat tcp-l:8546,fork,reuseaddr tcp:127.0.0.1:8545 &
+# avoid race
+sleep 1
+
+/usr/local/bin/erigon \
+    --datadir="$ERIGON_DATA_DIR" \
+    --log.console.verbosity="$VERBOSITY" \
+    --http \
+	--http.corsdomain="*" \
+	--http.vhosts="*" \
+    --http.addr=0.0.0.0 \
+    --http.port=8545 \
+    --http.api=admin,debug,eth,net,txpool,web3 \
+    --externalcl \
+    --ws \
+    --authrpc.jwtsecret="/hive/input/jwt-secret.txt" \
+    --authrpc.port=8551 \
+    --authrpc.addr=0.0.0.0 \
+    --nodiscover \
+    --no-downloader \
+    --maxpeers=0 \
+    --miner.gaslimit=$GAS_LIMIT
--- simulators/optimism/rpc/README.md
+++ simulators/optimism/rpc/README.md
@@ -4,4 +4,10 @@ This test suite is a copy of the ETH L1 RPC test suite adapted for Optimism L2.
 It tests several real-world scenarios such as sending value transactions,
 deploying a contract or interacting with one.
 
-    hive --sim optimism/rpc --client=go-ethereum,op-geth,op-proposer,op-batcher,op-node --docker.output
+If you use op-geth, use below command.
+
+`./hive --sim optimism/rpc --client=go-ethereum,op-geth,op-proposer,op-batcher,op-node --docker.output`
+
+If you use op-erigon, use below command
+
+`./hive --sim optimism/rpc --client=go-ethereum,op-erigon,op-proposer,op-batcher,op-node --docker.output`
