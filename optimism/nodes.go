6d6459e08ca6b0b9b9164c20c913e020874f5465
pcw109550 <pcw109550@gmail.com>
2023-05-16 15:39:06 +0900

    Add wrappers for p2p connection

--- optimism/nodes.go
+++ optimism/nodes.go
@@ -1,12 +1,17 @@
 package optimism
 
 import (
+	"context"
 	"crypto/ecdsa"
 	"fmt"
+
 	"github.com/ethereum-optimism/optimism/op-node/client"
 	"github.com/ethereum-optimism/optimism/op-node/p2p"
 	"github.com/ethereum-optimism/optimism/op-node/sources"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	gethp2p "github.com/ethereum/go-ethereum/p2p"
+	"github.com/ethereum/go-ethereum/p2p/enode"
 	"github.com/ethereum/hive/hivesim"
 )
 
@@ -63,6 +68,61 @@ type OpContracts struct {
 // OpL2Engine extends ELNode since it has all the same endpoints, except it is used for L2
 type OpL2Engine struct {
 	ELNode
+}
+
+// temporal extenstion of OpL2Engine
+// Direct RPC wrapper will be eventually moved to op-erigon or op-geth
+type OpL2EngineExtended struct {
+	*OpL2Engine
+}
+
+func (e *OpL2EngineExtended) NodeInfo(ctx context.Context) (*gethp2p.NodeInfo, error) {
+	var output *gethp2p.NodeInfo
+	err := e.RPC().CallContext(ctx, &output, "admin_nodeInfo")
+	return output, err
+
+}
+
+func (e *OpL2EngineExtended) Peers(ctx context.Context) (*gethp2p.PeerInfo, error) {
+	var output *gethp2p.PeerInfo
+	err := e.RPC().CallContext(ctx, &output, "admin_peers")
+	return output, err
+}
+
+func (e *OpL2EngineExtended) AddPeer(ctx context.Context, enode string) (bool, error) {
+	var output bool
+	err := e.RPC().CallContext(ctx, &output, "admin_addPeer", enode)
+	return output, err
+}
+
+// only interested in txhash for p2p testing
+// will be updated to use op-geth's RPCTransaction struct
+type RPCTransactionHash struct {
+	Hash common.Hash `json:"hash"`
+}
+
+func (e *OpL2EngineExtended) TxPoolContent(ctx context.Context) (map[string]map[string]map[string]*RPCTransactionHash, error) {
+	// only return necessary information(tx hash) for testing tx gossip
+	var output map[string]map[string]map[string]*RPCTransactionHash
+	err := e.RPC().CallContext(ctx, &output, "txpool_content")
+	return output, err
+}
+
+func (e *OpL2EngineExtended) ConnectPeer(ctx context.Context, neighbor *OpL2EngineExtended) error {
+	nodeInfo, err := neighbor.NodeInfo(ctx)
+	if err != nil {
+		return err
+	}
+	// sanity check by parsing result
+	_, err = enode.Parse(enode.ValidSchemes, nodeInfo.Enode)
+	if err != nil {
+		return err
+	}
+	_, err = e.AddPeer(ctx, nodeInfo.Enode)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 type OpNode struct {
